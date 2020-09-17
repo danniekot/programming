da631a0c0f64c457c6db62ab7c1cecf265fe52fb
Котляр Даниил Евгеньевич <dannie.kot@mail.ru>
2020-09-17 18:10:51 +0300

    1, 2 задания

---
+++ Практические/c++/01/01/01.cpp
@@ -0,0 +1,4 @@
+﻿#include <iostream>
+int main() {
+	std::cout << 2 + 2 * 2;
+}
---
+++ Практические/python/02.py
@@ -0,0 +1,5 @@
+a=3
+b=3.14
+print(a)
+print(b)
+print('А автоматически стала int, а В - float. Так работает Python.')
