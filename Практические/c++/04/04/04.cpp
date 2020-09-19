bb0e12efc510ea67d721121e823017f27e6dc428
Котляр Даниил Евгеньевич <dannie.kot@mail.ru>
2020-09-19 20:27:25 +0300

    programming

---
+++ Практические/c++/04/04/04.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	cout << "Введите два целых числа\n";
+	int a, b, temp;
+	cin >> a >> b;
+	temp = a; a = b; b = temp;
+	cout << "После замены первое число равно " << a << ", а второе " << b << "\n";
+	cout << "Введите два целых числа\n";
+	cin >> a >> b;
+	a += b; b = a - b; a -= b;
+	cout << "После замены первое число равно " << a << ", а второе " << b << "\n";
+}
