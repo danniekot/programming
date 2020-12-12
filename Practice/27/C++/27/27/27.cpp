e282564a83bc2b567d94fc85c2a165b3d3147571
Котляр Даниил Евгеньевич <dannie.kot@mail.ru>
2020-12-12 20:03:38 +0300

    all the works update

---
+++ Practice/27/C++/27/27/27.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+void pushSort(std::vector<int>& arr, int element) {
+	arr.push_back(element);
+	int j = arr.size() - 2;
+	while (j >= 0 && arr[j] < element) {
+		arr[j + 1] = arr[j];
+		j--;
+	}
+	arr[j + 1] = element;
+}
+template <typename T>
+void printVector(std::vector<T> arr, std::string before = "", std::string delim = " ") {
+	std::cout << before;
+	for (auto& it : arr)
+		std::cout << it << delim;
+}
+template <typename T>
+std::vector<T> sliceVector(std::vector<T> arr, int startIndex, int endIndex)
+{
+	size_t len = arr.size();
+	if (startIndex < 0)
+		startIndex = len + startIndex;
+	if (endIndex <= 0)
+		endIndex = len + endIndex - 1;
+	auto start = arr.begin() + startIndex;
+	auto end = arr.begin() + endIndex + 1;
+
+	std::vector<T> result(endIndex - startIndex + 1);
+	copy(start, end, result.begin());
+	return result;
+}
+int main() {
+	int n;
+	std::cout << "Введите число сигналов: ";
+	std::cin >> n;
+	int *signals = new int[n];
+	std::vector<int> visible;
+	int signal, i, j;
+	std::cout << "Массив сигналов: ";
+	for (i = 0; i < n; i++) {
+		std::cin >> signal;
+		signals[i] = signal;
+	}
+	for (i = 0; i < n; i++) {
+		signal = signals[i];
+		pushSort(visible, signal);
+		size_t visibleLen = visible.size();
+
+		printVector(sliceVector(visible, (visibleLen > 5 ? -5 : 0), 0));
+		std::cout << std::endl;
+	}
+}
---
+++ Practice/28/C++/28/28/28.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	int n;
+	cout << "Введите число: ";
+	cin >> n;
+	vector <pair<int, int>> ans;
+	for (int i = 2; i * i <= n; i++)
+		if (n % i == 0) {
+			ans.push_back(make_pair(i, 0));
+			while (n % i == 0) {
+				n /= i;
+				ans[ans.size() - 1].second++;
+			}
+		}
+	if (n > 1)
+		ans.push_back(make_pair(n, 1));
+	sort(ans.begin(), ans.end(), [](auto a, auto b) { return a.first < b.first; });
+	for (int i = 0; i < ans.size(); i++) {
+		auto divider = ans[i];
+		if (divider.second > 1) {
+			string format = (i == 0 ? "%d^%d" : "*%d^%d ");
+			cout << format.c_str(), ans[i].first, ans[i].second;
+		}
+		else {
+			string format = (i == 0 ? "%d" : "*%d");
+			cout << format.c_str(), ans[i].first;
+		}
+	}
+	cout << "\n";
+}
